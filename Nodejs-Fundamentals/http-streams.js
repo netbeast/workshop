8c473916064370596fb398a8ad098e01b2987971
franrios <fcojriosbello@gmail.com>
2016-04-28 07:11:27 +0200

    comments added

--- Nodejs-Fundamentals/http-streams.js
+++ Nodejs-Fundamentals/http-streams.js
@@ -1,12 +1,16 @@
 const http = require('http')
 
 http.createServer(function (req, res) {
-  // req is an http.IncomingMessage, which is a Readable Stream
-  // res is an http.ServerResponse, which is a Writable Stream
+  /*
+      req is an http.IncomingMessage, which is a Readable Stream
+      res is an http.ServerResponse, which is a Writable Stream
+  */
 
   var body = ''
-  // we want to get the data as utf8 strings
-  // If you don't set an encoding, then you'll get Buffer objects
+  /*
+      we want to get the data as utf8 strings
+      If you don't set an encoding, then you'll get Buffer objects
+  */
   req.setEncoding('utf8')
 
   // Readable streams emit 'data' events once a listener is added
