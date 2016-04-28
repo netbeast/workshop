f99e4229d8af84a975613a1dacd6f351163d92ae
franrios <fcojriosbello@gmail.com>
2016-04-28 07:10:13 +0200

    comments added

--- Nodejs-Fundamentals/hello-world-express.js
+++ Nodejs-Fundamentals/hello-world-express.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 
+// A request to IP:8000/ will be handled as follow:
 app.get('/', function (req, res) {
    res.send('Hello from Netbeast workshop!')
 })
