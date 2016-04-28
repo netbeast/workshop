093d2039c232df441a7015cb2d333dfa7a712027
franrios <fcojriosbello@gmail.com>
2016-04-28 07:11:38 +0200

    comments added

--- Nodejs-Fundamentals/server.js
+++ Nodejs-Fundamentals/server.js
@@ -4,11 +4,13 @@ var server = http.createServer().listen(8080, function () {
   console.log('Listening on port 8080...')
 })
 
+// Our server will handle the request as follow:
 server.on('request', function (req, res) {
   res.write('Hello from Node.js fundamentals!\n')
   res.end()
 })
 
+// It also notify on server side when a connection occurs
 server.on('connection', function (socket) {
   console.log('Hello from Node.js fundamentals! at server-side\n')
 })
