0565135f9fdfdbdffecc5cbe39d89b8749581d57
franrios <fcojriosbello@gmail.com>
2016-05-06 13:08:25 +0200

    in the middle added

--- Live-Examples/non-blocking.js
+++ Live-Examples/non-blocking.js
@@ -4,9 +4,12 @@ var fs = require('fs')
 
 fs.readFile('../README.md', function (err, asyncData) {
   if (err) throw err
-    console.log('1 ------------\n' + asyncData.toString())
+  console.log('1 ------------\n' + asyncData.toString())
 })
+
+console.log('\n\n################\n I am in the middle! is it blocked? \n################\n\n')
+
 fs.readFile('sampleFile', function (err, asyncData) {
   if (err) throw err
-    console.log('2 ------------\n' + asyncData.toString())
+  console.log('2 ------------\n' + asyncData.toString())
 })
