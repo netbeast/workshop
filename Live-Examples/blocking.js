70b7733575064640f63e6b5a54426b19bfe84369
franrios <fcojriosbello@gmail.com>
2016-05-06 13:08:15 +0200

    in the middle added

--- Live-Examples/blocking.js
+++ Live-Examples/blocking.js
@@ -6,4 +6,5 @@ var syncData2
 syncData1 = fs.readFileSync('../README.md').toString()
 syncData2 = fs.readFileSync('sampleFile').toString()
 console.log('1 ---------\n' + syncData1)
+console.log('\n\n################\n I am in the middle! is it blocked? \n################\n\n')
 console.log('2 ---------\n' + syncData2)
